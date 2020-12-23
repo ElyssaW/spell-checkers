25a460d9b5e5501d848c1911447c002c4a4c007c
ElyssaW <ejwinch1@gmail.com>
2020-12-23 13:14:55 -0600

    created draw functions for three rooms, as well as functions to change expected string input between them. Player can now approach door, input string, and move to the next room if correct, for all three rooms!

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ console.log('Hello!')// reference DOM elements
 let ctx = game.getContext('2d')
 let frame = 0
 let playerHealth = 0
+let roomIndex = 0
+let compString
+let gameInterval
+let gameStart = false
 let nearDoor = false
 let doorUnlocked = false
 
@@ -37,12 +41,47 @@ game.setAttribute('height', getComputedStyle(game)['height'])
 //
 //===============================================
 
+// Initialize door counter
+function renderDoorText(roomIndex) {
+    switch(roomIndex) {
+        case 0:
+            doorOne()
+            break;
+        case 1:
+            doorTwo()
+            break;
+        case 2:
+            doorThree()
+            break;
+    }
+}
+
 // Initialize functions to set the door text
 function doorOne () {
     if (nearDoor === false) {
         document.querySelector('#firstHalf').innerText = 'Her Highness\' Royal And '
         document.querySelector('#typo').innerText = 'Msot'
         document.querySelector('#secondHalf').innerText = ' Perfect Essay on The History of Spells'
+        nearDoor = true
+    } 
+}
+
+// Initialize functions to set the door text
+function doorTwo () {
+    if (nearDoor === false) {
+        document.querySelector('#firstHalf').innerText = 'And here is the '
+        document.querySelector('#typo').innerText = 'azginma'
+        document.querySelector('#secondHalf').innerText = ' second sentence'
+        nearDoor = true
+    } 
+}
+
+// Initialize functions to set the door text
+function doorThree () {
+    if (nearDoor === false) {
+        document.querySelector('#firstHalf').innerText = 'And finally, the '
+        document.querySelector('#typo').innerText = 'mnecitnifiag'
+        document.querySelector('#secondHalf').innerText = ' second sentence'
         nearDoor = true
     } 
 }
@@ -55,10 +94,10 @@ function clearText () {
 }
 
 // If input matches expected string, unlock door
-function compareString(input) {
-    let string = 'Most'
-    if (input == string) {
+function compareString(input, compString) {
+    if (input == compString) {
         doorUnlocked = true
+        door.color = 'white'
     } else {
         console.log('Booooo')
     }
@@ -79,7 +118,7 @@ document.querySelector('form').addEventListener('submit', (e)=> {
     textInput.value = ''
     
     if (nearDoor) {
-        compareString(playerInput)
+        compareString(playerInput, compString)
     }
 })
 
@@ -89,6 +128,13 @@ document.querySelector('form').addEventListener('submit', (e)=> {
 //
 //================================================
 
+// Initialize persistent objects
+let door
+let doorMat
+let doorMatEdge
+let hero
+
+// Constructor function for new objects
 function Constructor(x, y, color, width, height) {
     this.x = x
     this.y = y
@@ -105,11 +151,31 @@ function Constructor(x, y, color, width, height) {
     }
 }
 
-let door = new Constructor(850, 40, 'red', 60, 60)
-let doorMat = new Constructor(door.x-((door.width)/2), door.y-((door.height)/2), 'blue', 120, 120)
-let doorMatEdge = new Constructor(door.x-((door.width)/2)-15, door.y-((door.height)/2)-15, 'green', 150, 150)
+// Generates new door + door adjacent objects
+function generateDoor () { 
+    door = new Constructor(850, 40, 'red', 60, 60)
+    doorMat = new Constructor(door.x-((door.width)/2), door.y-((door.height)/2), 'blue', 120, 120)
+    doorMatEdge = new Constructor(door.x-((door.width)/2)-15, door.y-((door.height)/2)-15, 'green', 150, 150)
+}
 
-let hero = new Constructor(150, 150, 'hotpink', 60, 60)
+// Generates new player. Should only run on game start
+function generatePlayer () {
+    hero = new Constructor(150, 150, 'hotpink', 60, 60)
+}
+
+function placeDoorLocked (x, y) { 
+    // Move door, doormat, and doormat edge to
+    // proper place
+    door.x = x
+    door.y = y
+    doorMat.x = door.x-(door.width/2)
+    doorMat.y = door.y-(door.height/2)
+    doorMatEdge.x = door.x-(door.width/2)-15
+    doorMatEdge.y = door.y-(door.height/2)-15
+    
+    // Set door to locked
+    door.alive = true
+}
 
 // Make object walk in random direction
 function randomWalk (obj) {
@@ -187,62 +253,6 @@ function wallCheck(obj) {
     }
 }
 
-let gameLoop = () => {
-    //Clear board
-    ctx.clearRect(0, 0, game.width, game.height)
-    
-    // Increment frame
-    frame++
-    
-    // Move player
-    movementHandler(keyValue)
-    
-    if (detectHit(doorMat)) {
-        doorOne()
-    } else if (detectHit(doorMatEdge)) {
-        clearText()
-    }
-    
-    if (detectHit(door) && doorUnlocked) {
-        console.log('Move to next room')
-    } else if (detectHit(door)) {
-        
-    }
-    
-    // Check if player is going over the border
-    if (hero.x < 0) {
-        hero.x = 0
-    } else if (hero.x+hero.width > game.width) {
-        hero.x = game.width  - hero.width
-    }
-    
-    if (hero.y < 0) {
-        hero.y = 0
-    } else if (hero.y+hero.height > game.height) {
-        hero.y = game.height  - hero.height
-    }
-
-//    // check if Ogre is alive
-//    if (ogre.alive) {
-//        // Render Ogre
-//        ogre.render()
-//        
-//        // Detect hit
-//        if (detectHit(ogre)) {
-//            ogre.alive = false
-//        }
-//        
-//        // Move ogre and check for wall
-//        objectWalk(ogre)
-//        wallCheck(ogre)
-//    }
-    // Render hero
-    hero.render()
-    doorMatEdge.render()
-    doorMat.render()
-    door.render()
-}
-
 // Four corner collision detection
 let detectHit = (obj) => {
         // Check top left corner
@@ -286,6 +296,51 @@ let movementHandler = e => {
     }
 }
 
+function moveToNextRoom (roomIndex) {
+    switch(roomIndex) {
+        case 0:
+            
+            break;
+        case 1:
+            drawSecondRoom()
+            break;
+        case 2:
+            drawThirdRoom()
+            break;
+        case 3:
+            console.log('You won!')
+    }
+}
+
+function drawSecondRoom () {
+    // Place new locked door
+    placeDoorLocked(400, 400)
+    compString = 'amazing'
+    doorUnlocked = false
+    door.color = 'red'
+    
+    // Place player
+    hero.x = 40
+    hero.y = 40
+}
+
+function drawThirdRoom () {
+    // Place new locked door
+    placeDoorLocked(600, 200)
+    compString = 'magnificient'
+    doorUnlocked = false
+    door.color = 'red'
+    
+    // Place player
+    hero.x = 40
+    hero.y = 40
+}
+
+// Draw Room 
+// Room number
+    // Draw door back
+    // Draw door forward
+
 // This duo of functions essentially replicates the keypress events for the arrow keys.
 // The typical keypress event gets interrupted by typing - meaning, the hero will stop
 // moving when the player types, until the player moves them again. This tracks the arrowkeys
@@ -310,6 +365,76 @@ document.addEventListener('keydown', e => {
          }
  })
 
-let gameInterval = setInterval(gameLoop, 30)
+let gameLoop = () => {
+    
+    //Clear board
+    ctx.clearRect(0, 0, game.width, game.height)
+    
+    // Increment frame
+    frame++
+    
+    // Move player
+    movementHandler(keyValue)
+    
+    if (detectHit(doorMat)) {
+        renderDoorText(roomIndex)
+    } else if (detectHit(doorMatEdge)) {
+        clearText()
+    }
+    
+    if (detectHit(door) && doorUnlocked) {
+        console.log('Move to next room')
+        door.alive = true
+        clearText()
+        roomIndex++
+        moveToNextRoom(roomIndex)
+    } else if (detectHit(door)) {
+        
+    }
+    
+    // Check if player is going over the border
+    if (hero.x < 0) {
+        hero.x = 0
+    } else if (hero.x+hero.width > game.width) {
+        hero.x = game.width  - hero.width
+    }
+    
+    if (hero.y < 0) {
+        hero.y = 0
+    } else if (hero.y+hero.height > game.height) {
+        hero.y = game.height  - hero.height
+    }
 
+//    // check if Ogre is alive
+//    if (ogre.alive) {
+//        // Render Ogre
+//        ogre.render()
+//        
+//        // Detect hit
+//        if (detectHit(ogre)) {
+//            ogre.alive = false
+//        }
+//        
+//        // Move ogre and check for wall
+//        objectWalk(ogre)
+//        wallCheck(ogre)
+//    }
+    // Render hero
+    doorMatEdge.render()
+    doorMat.render()
+    door.render()
+    
+    hero.render()
+}
+
+function roomStart () {
+    console.log('Hello again')
+    generateDoor()
+    generatePlayer()
+    gameInterval = setInterval(gameLoop, 30)
+    compString = 'Most'
+    gameStart = true
+}
+
+roomStart()
 
