69536d6d4d77fe1523771cb67fa87b2be7091c1e
ElyssaW <ejwinch1@gmail.com>
2021-01-04 16:34:53 -0600

    Moved the movement input functions to switch statements

--- js/input.js
+++ js/input.js
@@ -43,9 +43,7 @@ function movementHandler () {
 // The typical keypress event gets interrupted by typing - meaning, the hero will stop
 // moving when the player types, until the player moves them again. This tracks the numpad
 // by keydown/keyup instead, so that the computer will continue accepting movement input even
-// while the keyboard is being used for typing. Strangely, switch statements seemed to cause
-// the keydown/keyup events to sometimes cancel each other out, so I used a list of If statements
-// instead
+// while the keyboard is being used for typing. 
 document.addEventListener('keydown', e => {
     
     // Check if player is backspacing on current text
@@ -61,91 +59,105 @@ document.addEventListener('keydown', e => {
         // Move player
         hero.x = hero.x + (hero.xdir * 200)
         hero.y = hero.y + (hero.ydir * 200)
+        
     } else {
         // If it is a numpad press, interact with move object accordingly
-         if (e.key == '8') {
-                 moveObject.up = true
-                 hero.ydir = -1
-             } 
-        if (e.key == '2') {
-                 moveObject.down = true
-                 hero.ydir = 1
-             } 
-        if (e.key == '4') {
-                 moveObject.left = true
-                 hero.sprite = document.getElementById("madgeFlipped")
-                 hero.xdir = -1
-             } 
-        if (e.key == '6') {
-                 moveObject.right = true
-                 hero.sprite = document.getElementById("madge")
-                 hero.xdir = 1
-             }
-        if (e.key == '3') {
-                 moveObject.bottomright = true
-                 hero.sprite = document.getElementById("madge")
-                 hero.ydir = 1
-                 hero.xdir = 1
-             } 
-        if (e.key == '1') {
-                 moveObject.bottomleft = true
-                 hero.sprite = document.getElementById("madgeFlipped")
-                 hero.ydir = 1
-                 hero.xdir = -1
-             } 
-        if (e.key == '7') {
-                 moveObject.topleft = true
-                 hero.sprite = document.getElementById("madgeFlipped")
-                 hero.ydir = -1
-                 hero.xdir = -1
-             } 
-        if (e.key == '9') {
-                 moveObject.topright = true
-                 hero.sprite = document.getElementById("madge")
-                 hero.ydir = -1
-                 hero.xdir =  1
-             }
+        switch (e.key) {
+            case '1':
+                moveObject.bottomleft = true
+                hero.sprite = document.getElementById("madgeFlipped")
+                break;
+                
+            case '2':
+                moveObject.down = true
+                break;
+                
+            case '3':
+                moveObject.bottomright = true
+                hero.sprite = document.getElementById("madge")
+                break;
+                
+            case '4':
+                moveObject.left = true
+                hero.sprite = document.getElementById("madgeFlipped")
+                break;
+                
+            case '6':
+                moveObject.right = true
+                hero.sprite = document.getElementById("madge")
+                break;
+                
+            case '7':
+                moveObject.topleft = true
+                hero.sprite = document.getElementById("madgeFlipped")
+                break;
+                
+            case '8':
+                moveObject.up = true
+                break;
+                
+            case '9':
+                moveObject.topright = true
+                hero.sprite = document.getElementById("madge")
+                break;
+            }
     }
  })
 
 // Listen for key up, to stop moving the player
  document.addEventListener('keyup', e => {
-     if (e.key == '8') {
-             moveObject.up = false
-             hero.ydir = 0
-         } 
-    if (e.key == '2') {
-             moveObject.down = false
-             hero.ydir = 0
-         } 
-    if (e.key == '4') {
-             moveObject.left = false
-             hero.xdir = 0
-         } 
-    if (e.key == '6') {
-             moveObject.right = false
-             hero.xdir = 0
-         }
-     if (e.key == '3') {
-             moveObject.bottomright = false
-             hero.ydir = 0
-             hero.xdir = 0
-         } 
-    if (e.key == '1') {
-             moveObject.bottomleft = false
-             hero.ydir = 0
-             hero.xdir = 0
-         } 
-    if (e.key == '7') {
-             moveObject.topleft = false
-             hero.ydir = 0
-             hero.xdir = 0
-         } 
-    if (e.key == '9') {
-             moveObject.topright = false
-             hero.ydir =  0
-             hero.xdir =  0
-         }
+     // If it is a numpad press, interact with move object accordingly
+        switch (e.key) {
+            case '1':
+                moveObject.bottomleft = false
+                hero.sprite = document.getElementById("madgeFlipped")
+                hero.ydir = 0
+                hero.xdir = 0
+                break;
+                
+            case '2':
+                moveObject.down = false
+                hero.ydir = 0
+                break;
+                
+            case '3':
+                moveObject.bottomright = false
+                hero.sprite = document.getElementById("madge")
+                hero.ydir = 0
+                hero.xdir = 0
+                break;
+                
+            case '4':
+                moveObject.left = false
+                hero.sprite = document.getElementById("madgeFlipped")
+                hero.xdir = 0
+                break;
+                
+            case '6':
+                moveObject.right = false
+                hero.sprite = document.getElementById("madge")
+                hero.xdir = 0
+                break;
+                
+            case '7':
+                moveObject.topleft = false
+                hero.sprite = document.getElementById("madgeFlipped")
+                hero.ydir = 0
+                hero.xdir = 0
+                break;
+                
+            case '8':
+                moveObject.up = false
+                hero.ydir = 0
+                break;
+                
+            case '9':
+                moveObject.topright = false
+                hero.sprite = document.getElementById("madge")
+                hero.ydir = 0
+                hero.xdir = 0
+                break;
+            }
  })
 
 // Function to check player input when Enter is pressed
